67b22fcdfcf95d0b8eb5c2c3c48f0cc6d3e0179a
swarani <151661365+swarani6@users.noreply.github.com>
2024-03-21 15:48:46 +0530

    Create WordSearch.py

---
+++ WordSearch.py
@@ -0,0 +1,31 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        ROWS, COLS = len(board), len(board[0])
+        visited = set()
+
+        def dfs(r,c,idx):
+            if idx == len(word):
+                return True
+            if ( 
+                r<0 or r>=ROWS 
+                or c<0 or c>=COLS
+                or word[idx] != board[r][c]
+                or (r,c) in visited
+            ):
+                return False
+            visited.add((r,c))
+            res = (
+                dfs(r+1,c,idx+1) 
+                or dfs(r-1,c,idx+1) 
+                or dfs(r,c+1,idx+1) 
+                or dfs(r,c-1,idx+1)
+            )
+        
+            visited.remove((r,c))
+            return res
+        
+        for i in range(ROWS):
+            for j in range(COLS):
+                if dfs(i,j,0):
+                    return True
+        return False
