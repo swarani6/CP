deba772297790c0c325747ffb066fee11387371b
swarani <151661365+swarani6@users.noreply.github.com>
2024-03-21 21:18:22 +0530

    Create FindthePivotInteger.py

---
+++ FindthePivotInteger.py
@@ -0,0 +1,9 @@
+class Solution:
+    def pivotInteger(self, n: int) -> int:
+        x = sqrt(n * (n + 1) / 2)
+
+        if x % 1 != 0:
+            return -1
+        else:
+            return int(x)
+        
